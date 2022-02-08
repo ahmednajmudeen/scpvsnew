f9671797b8bbb783b5af5c210ebae5729096c321
ahmednajmudeen <80084554+ahmednajmudeen@users.noreply.github.com>
2022-02-08 22:38:44 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,25 +19,25 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Link Hosting Kalian Untuk Ssh Vpn
-akbarvpn="raw.githubusercontent.com/Nurjanah1981/scriptvps/main/ssh"
+akbarvpn="raw.githubusercontent.com/ahmednajmudeen/scpvsnew/ssh"
 # Link Hosting Kalian Untuk Sstp
-akbarvpnn="raw.githubusercontent.com/Nurjanah1981/scriptvps/main/sstp"
+akbarvpnn="raw.githubusercontent.com/ahmednajmudeen/scpvsnew/sstp"
 # Link Hosting Kalian Untuk Ssr
-akbarvpnnn="raw.githubusercontent.com/Nurjanah1981/scriptvps/main/ssr"
+akbarvpnnn="raw.githubusercontent.com/ahmednajmudeen/scpvsnew/ssr"
 # Link Hosting Kalian Untuk Shadowsocks
-akbarvpnnnn="raw.githubusercontent.com/Nurjanah1981/scriptvps/main/shadowsocks"
+akbarvpnnnn="raw.githubusercontent.com/ahmednajmudeen/scpvsnew/shadowsocks"
 # Link Hosting Kalian Untuk Wireguard
-akbarvpnnnnn="raw.githubusercontent.com/Nurjanah1981/scriptvps/main/wireguard"
+akbarvpnnnnn="raw.githubusercontent.com/ahmednajmudeen/scpvsnew/wireguard"
 # Link Hosting Kalian Untuk Xray
-akbarvpnnnnnn="raw.githubusercontent.com/Nurjanah1981/scriptvps/main/xray"
+akbarvpnnnnnn="raw.githubusercontent.com/ahmednajmudeen/scpvsnew/xray"
 # Link Hosting Kalian Untuk Ipsec
-akbarvpnnnnnnn="raw.githubusercontent.com/Nurjanah1981/scriptvps/main/ipsec"
+akbarvpnnnnnnn="raw.githubusercontent.com/ahmednajmudeen/scpvsnew/ipsec"
 # Link Hosting Kalian Untuk Backup
-akbarvpnnnnnnnn="raw.githubusercontent.com/Nurjanah1981/scriptvps/main/backup"
+akbarvpnnnnnnnn="raw.githubusercontent.com/ahmednajmudeen/scpvsnew/backup"
 # Link Hosting Kalian Untuk Websocket
-akbarvpnnnnnnnnn="raw.githubusercontent.com/Nurjanah1981/scriptvps/main/websocket"
+akbarvpnnnnnnnnn="raw.githubusercontent.com/ahmednajmudeen/scpvsnew/websocket"
 # Link Hosting Kalian Untuk Ohp
-akbarvpnnnnnnnnnn="raw.githubusercontent.com/Nurjanah1981/scriptvps/main/ohp"
+akbarvpnnnnnnnnnn="raw.githubusercontent.com/ahmednajmudeen/scpvsnew/ohp"
 
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
