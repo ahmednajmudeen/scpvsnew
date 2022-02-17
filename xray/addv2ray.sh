4bc7bfd4996ccb77ee0090fff78ef01a4b7286c9
ahmednajmudeen <80084554+ahmednajmudeen@users.noreply.github.com>
2022-02-18 03:52:34 +0800

    Update addv2ray.sh

--- xray/addv2ray.sh
+++ xray/addv2ray.sh
@@ -13,20 +13,8 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Nurjanah1981/perizinan/main/ipvps.txt | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/lis.tio.718"
-echo -e "${NC}${LIGHT}WhatsApp : 081545854516"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/Akbar218"
-exit 0
-fi
-clear
-source /var/lib/akbarstorevpn/ipvps.conf
+IP=$(wget -qO- ipinfo.io/ip);
+
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/xray/domain)
 else
